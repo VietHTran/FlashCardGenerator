53890208db6605488a2cbf2311a77a6c9ce728fc
VietHTran <thanos.trn11@gmail.com>
2016-11-12 10:03:41 -0600

    Enable Load Data From Database

--- FlashCard/app/src/main/java/com/example/android/flashcard/MainActivity.java
+++ FlashCard/app/src/main/java/com/example/android/flashcard/MainActivity.java
@@ -5,9 +5,13 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 import android.view.Menu;
 import android.view.MenuItem;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,10 +21,24 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
-        DataManager dataManager= new DataManager();
-
+        initData();
     }
-
+    private void initData(){
+        DataManager dataManager= new DataManager();
+        FlashcardLab lab = FlashcardLab.get(this);
+        List<FlashcardHolder> list=lab.getFlashcards();
+        for (int i=0;i<list.size();i++) {
+            FlashcardHolder holder= list.get(i);
+            FlashCard flashCard=new FlashCard(holder.question,holder.answer,holder.collection,holder.uuid);
+            if (!DataManager.collections.containsKey(holder.collection)) {
+                Collection collection= new Collection(holder.collection);
+                collection.flashCards.add(flashCard);
+                DataManager.collections.put(holder.collection,collection);
+            } else {
+                DataManager.collections.get(holder.collection).flashCards.add(flashCard);
+            }
+        }
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
