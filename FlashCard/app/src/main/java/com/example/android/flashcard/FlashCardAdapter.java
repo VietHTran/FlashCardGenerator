46649fb136b9bd967cce8007b25b0ccdc1b76ba6
VietHTran <thanos.trn11@gmail.com>
2016-11-12 06:25:59 -0600

    Add FlashCardAdapter

--- FlashCard/app/src/main/java/com/example/android/flashcard/FlashCardAdapter.java
+++ FlashCard/app/src/main/java/com/example/android/flashcard/FlashCardAdapter.java
@@ -5,7 +5,6 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
-import android.widget.ImageView;
 import android.widget.TextView;
 
 import java.util.List;
@@ -13,16 +12,16 @@ import java.util.List;
 /**
  * Created by Viet on 11/12/2016.
  */
-public class CollectionAdapter extends ArrayAdapter<Collection> {
-    public CollectionAdapter(Activity context, List<Collection> collections) {
-        super(context,0,collections);
+public class FlashCardAdapter extends ArrayAdapter<FlashCard> {
+    public FlashCardAdapter(Activity context, List<FlashCard> flashCards) {
+        super(context,0,flashCards);
     }
     @Override
     public View getView(int pos, View convertView, ViewGroup parent) {
-        Collection i=getItem(pos);
-        View root= LayoutInflater.from(getContext()).inflate(R.layout.list_item_collection,parent,false);
+        FlashCard i=getItem(pos);
+        View root= LayoutInflater.from(getContext()).inflate(R.layout.list_item_flash_card,parent,false);
         TextView textView=(TextView) root;
-        textView.setText(i.name);
+        textView.setText(i.question);
         return root;
     }
 }
