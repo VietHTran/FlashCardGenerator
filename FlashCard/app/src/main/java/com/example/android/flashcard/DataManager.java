42834e469b38a08d2568328bf115d46ba641124b
VietHTran <thanos.trn11@gmail.com>
2016-11-12 05:55:11 -0600

    Create DataManager

---
+++ FlashCard/app/src/main/java/com/example/android/flashcard/DataManager.java
@@ -0,0 +1,13 @@
+package com.example.android.flashcard;
+
+import java.util.HashMap;
+
+/**
+ * Created by Viet on 11/12/2016.
+ */
+public class DataManager {
+    public static HashMap<String,Collection> collections;
+    public DataManager() {
+        collections=new HashMap<String, Collection>();
+    }
+}
