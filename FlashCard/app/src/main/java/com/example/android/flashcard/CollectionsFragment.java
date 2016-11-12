fc9e773f25d5f87789026744dc418692a9084346
VietHTran <thanos.trn11@gmail.com>
2016-11-12 06:02:41 -0600

    Add Menu To CollectionsActivity

--- FlashCard/app/src/main/java/com/example/android/flashcard/CollectionsFragment.java
+++ FlashCard/app/src/main/java/com/example/android/flashcard/CollectionsFragment.java
@@ -3,8 +3,13 @@ package com.example.android.flashcard;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
+
+import java.util.ArrayList;
 
 /**
  * A placeholder fragment containing a simple view.
@@ -13,7 +18,27 @@ public class CollectionsFragment extends Fragment {
 
     public CollectionsFragment() {
     }
-
+    //Setup the menu call by setHasOptionsMenu(true);
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater menuInflater) {
+        super.onCreateOptionsMenu(menu,menuInflater);
+        menuInflater.inflate(R.menu.menu_collections_fragment,menu);
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id=item.getItemId();
+        //Add new collection
+        if (id==R.id.action_add) {
+            //Create popup menu
+            return true;
+        }
+        return true;
+    }
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setHasOptionsMenu(true);
+    }
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
