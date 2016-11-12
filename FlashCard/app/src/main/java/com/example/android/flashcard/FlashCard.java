921e0ed72f4c45e10326d5de42495fdc1e2f39f6
VietHTran <thanos.trn11@gmail.com>
2016-11-12 03:59:07 -0600

    Add Layout For Single Collection

---
+++ FlashCard/app/src/main/java/com/example/android/flashcard/FlashCard.java
@@ -0,0 +1,8 @@
+package com.example.android.flashcard;
+
+/**
+ * Created by Viet on 11/12/2016.
+ */
+public class FlashCard {
+
+}
--- FlashCard/app/src/main/java/com/example/android/flashcard/MainActivityFragment.java
+++ FlashCard/app/src/main/java/com/example/android/flashcard/MainActivityFragment.java
@@ -1,15 +1,19 @@
 package com.example.android.flashcard;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+    Button collectionButton;
+    Button reviewButton;
 
     public MainActivityFragment() {
     }
@@ -17,6 +21,24 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+        View view =  inflater.inflate(R.layout.fragment_main, container, false);
+
+        collectionButton = (Button) view.findViewById(R.id.collection_main);
+        collectionButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent=new Intent(getActivity(),Collections.class);
+                startActivity(intent);
+            }
+        });
+
+        reviewButton = (Button) view.findViewById(R.id.review_main);
+        reviewButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+            }
+        });
+
+        return view;
     }
 }
