6e4f36f115db7825a9ce283d4f546aede1ec8ead
VietHTran <thanos.trn11@gmail.com>
2016-11-13 00:01:47 -0600

    Change Icon

--- FlashCard/app/src/main/java/com/example/android/flashcard/Utility.java
+++ FlashCard/app/src/main/java/com/example/android/flashcard/Utility.java
@@ -1,6 +1,7 @@
 package com.example.android.flashcard;
 
 import android.content.Context;
+import android.content.Intent;
 import android.widget.Toast;
 
 /**
@@ -8,6 +9,25 @@ import android.widget.Toast;
  */
 public class Utility {
     public static void handlingMessage(Context context,String message) {
-        Toast.makeText(context,"Error: Unable to recognize voice", Toast.LENGTH_SHORT).show();
+        String[] words=message.split(" ");
+        if (words.length==0) {
+            Toast.makeText(context,"Error: Unable to recognize voice", Toast.LENGTH_SHORT).show();
+        }
+        for (int i=0;i<words.length;i++) {
+            words[i].toLowerCase();
+        }
+        if (words[0].equals("review")){
+            Intent intent= new Intent(context,ReviewActivity.class);
+            context.startActivity(intent);
+        } else if (words[0].equals("collections")) {
+            Intent intent= new Intent(context,CollectionsActivity.class);
+            context.startActivity(intent);
+        } else if (DataManager.collections.containsKey(words[0])) {
+            Intent intent= new Intent(context,SingleCollectionActivity.class);
+            intent.putExtra("title",words[0]);
+            context.startActivity(intent);
+        } else {
+            Toast.makeText(context,"Error: Unable to understand command", Toast.LENGTH_SHORT).show();
+        }
     }
 }
