ce580eaaa765fd5d831070eab2812429e2ed68fe
VietHTran <thanos.trn11@gmail.com>
2016-11-19 04:04:05 -0600

    Add Voice Input For CardActivity

--- FlashCard/app/src/main/java/com/example/android/flashcard/CardActivityFragment.java
+++ FlashCard/app/src/main/java/com/example/android/flashcard/CardActivityFragment.java
@@ -1,8 +1,12 @@
 package com.example.android.flashcard;
 
+import android.app.Activity;
+import android.app.SearchManager;
 import android.content.Intent;
+import android.speech.RecognizerIntent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -12,13 +16,14 @@ import android.widget.Toast;
 
 import org.w3c.dom.Text;
 
+import java.util.ArrayList;
 import java.util.UUID;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class CardActivityFragment extends Fragment {
-
+    TextView question,answer;
     public CardActivityFragment() {
     }
 
@@ -26,8 +31,8 @@ public class CardActivityFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         View rootView=inflater.inflate(R.layout.fragment_card, container, false);
-        final TextView question=(TextView) rootView.findViewById(R.id.input_question);
-        final TextView answer=(TextView) rootView.findViewById(R.id.input_answer);
+        question=(TextView) rootView.findViewById(R.id.input_question);
+        answer=(TextView) rootView.findViewById(R.id.input_answer);
         Button save =(Button) rootView.findViewById(R.id.card_save);
         save.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -58,6 +63,77 @@ public class CardActivityFragment extends Fragment {
                 getActivity().finish();
             }
         });
+        Button voiceInput=(Button)rootView.findViewById(R.id.card_voice);
+        voiceInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                speak(v);
+            }
+        });
         return rootView;
+    }
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == MainActivityFragment.VOICE_RECOGNITION_REQUEST_CODE)
+
+            //If Voice recognition is successful then it returns RESULT_OK
+            if(resultCode == Activity.RESULT_OK) {
+
+                ArrayList<String> textMatchList = data
+                        .getStringArrayListExtra(RecognizerIntent.EXTRA_RESULTS);
+
+                if (!textMatchList.isEmpty()) {
+                    // If first Match contains the 'search' word
+                    // Then start web search.
+                    if (textMatchList.get(0).contains("search")) {
+
+                        String searchQuery = textMatchList.get(0);
+                        searchQuery = searchQuery.replace("search","");
+                        Intent search = new Intent(Intent.ACTION_WEB_SEARCH);
+                        search.putExtra(SearchManager.QUERY, searchQuery);
+                        startActivity(search);
+                    } else {
+                        String res=textMatchList.get(0);
+                        Log.v("test","thisisquestion "+question);
+                        if (question.getText().toString().trim().equals("")){
+                            question.setText(res);
+                        } else {
+                            answer.append(res);
+                        }
+                    }
+
+                }
+                //Result code for various error.
+            }else if(resultCode == RecognizerIntent.RESULT_AUDIO_ERROR){
+                Utility.showToastMessage(getActivity(),"Audio Error");
+            }else if(resultCode == RecognizerIntent.RESULT_CLIENT_ERROR){
+                Utility.showToastMessage(getActivity(),"Client Error");
+            }else if(resultCode == RecognizerIntent.RESULT_NETWORK_ERROR){
+                Utility.showToastMessage(getActivity(),"Network Error");
+            }else if(resultCode == RecognizerIntent.RESULT_NO_MATCH){
+                Utility.showToastMessage(getActivity(),"No Match");
+            }else if(resultCode == RecognizerIntent.RESULT_SERVER_ERROR){
+                Utility.showToastMessage(getActivity(),"Server Error");
+            }
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+    public void speak(View view) {
+        Intent intent = new Intent(RecognizerIntent.ACTION_RECOGNIZE_SPEECH);
+
+        // Specify the calling package to identify your application
+        intent.putExtra(RecognizerIntent.EXTRA_CALLING_PACKAGE, getClass()
+                .getPackage().getName());
+
+        // Given an hint to the recognizer about what the user is going to say
+        //There are two form of language model available
+        //1.LANGUAGE_MODEL_WEB_SEARCH : For short phrases
+        //2.LANGUAGE_MODEL_FREE_FORM  : If not sure about the words or phrases and its domain.
+        intent.putExtra(RecognizerIntent.EXTRA_LANGUAGE_MODEL,
+                RecognizerIntent.LANGUAGE_MODEL_WEB_SEARCH);
+        // Specify how many results you want to receive. The results will be
+        // sorted where the first result is the one with higher confidence.
+        intent.putExtra(RecognizerIntent.EXTRA_MAX_RESULTS, 1);
+        //Start the Voice recognizer activity for the result.
+        startActivityForResult(intent,MainActivityFragment.VOICE_RECOGNITION_REQUEST_CODE);
     }
 }
